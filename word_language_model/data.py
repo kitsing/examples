ff459086344bb926a9738307ec222055c87a7d9f
Chu-Cheng Lin <chu.cheng.lin@gmail.com>
2019-12-12 11:17:35 -0800

    sync

--- word_language_model/data.py
+++ word_language_model/data.py
@@ -20,9 +20,15 @@ class Dictionary(object):
 class Corpus(object):
     def __init__(self, path):
         self.dictionary = Dictionary()
-        self.train = self.tokenize(os.path.join(path, 'train.txt'))
-        self.valid = self.tokenize(os.path.join(path, 'valid.txt'))
-        self.test = self.tokenize(os.path.join(path, 'test.txt'))
+        self.dictionary.add_word('<pad>')
+        self.train = self.tokenize(os.path.join(path, 'train.txt.r'))
+        self.valid = self.tokenize(os.path.join(path, 'valid.txt.r'))
+        self.test = self.tokenize(os.path.join(path, 'test.txt.r'))
+        magic_size = 50265
+        counter = 0
+        while len(self.dictionary) < magic_size:
+            self.dictionary.add_word(counter)
+            counter += 1
 
     def tokenize(self, path):
         """Tokenizes a text file."""
@@ -30,19 +36,21 @@ class Corpus(object):
         # Add words to the dictionary
         with open(path, 'r', encoding="utf8") as f:
             for line in f:
-                words = line.split() + ['<eos>']
+                words = line.split(' ') + ['<eos>']
                 for word in words:
                     self.dictionary.add_word(word)
-
+        print(f'dict length: {len(self.dictionary)}')
+        max_len=800
         # Tokenize file content
         with open(path, 'r', encoding="utf8") as f:
             idss = []
             for line in f:
-                words = line.split() + ['<eos>']
+                words = line.strip().split(' ') + ['<eos>']
                 ids = []
                 for word in words:
                     ids.append(self.dictionary.word2idx[word])
-                idss.append(torch.tensor(ids).type(torch.int64))
+                remaining = ['<pad>'] * (max_len - len(ids))
+                idss.append(torch.tensor(ids+remaining).type(torch.int64))
             ids = torch.cat(idss)
 
         return ids
