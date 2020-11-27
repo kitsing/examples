5185d5e48b35985f8076ec32e7e5aeb5332650a7
Chu-Cheng Lin <chu.cheng.lin@gmail.com>
2020-11-27 02:26:58 -0500

    checkpoint

--- word_language_model/data.py
+++ word_language_model/data.py
@@ -10,7 +10,7 @@ class Dictionary(object):
     def add_word(self, word):
         if word not in self.word2idx:
             self.idx2word.append(word)
-            self.word2idx[word] = int(word)
+            self.word2idx[word] = len(self.idx2word) - 1
         return self.word2idx[word]
 
     def __len__(self):
@@ -18,17 +18,16 @@ class Dictionary(object):
 
 
 class Corpus(object):
+    padding = '<pad>'
+    bos = '<bos>'
+    eos = '<eos>'
     def __init__(self, path):
         self.dictionary = Dictionary()
-        self.dictionary.add_word('1')
-        self.train = self.tokenize(os.path.join(path, 'train.txt.r'))
-        self.valid = self.tokenize(os.path.join(path, 'valid.txt.r'))
-        self.test = self.tokenize(os.path.join(path, 'test.txt.r'))
-        magic_size = 50265
-        counter = 0
-        while len(self.dictionary) < magic_size:
-            self.dictionary.add_word(str(counter))
-            counter += 1
+        for w in [Corpus.padding, Corpus.bos, Corpus.eos]:
+            self.dictionary.add_word(w)
+        self.train = self.tokenize(os.path.join(path, 'train.txt'))
+        self.valid = self.tokenize(os.path.join(path, 'valid.txt'))
+        self.test = self.tokenize(os.path.join(path, 'test.txt'))
 
     def tokenize(self, path):
         """Tokenizes a text file."""
@@ -41,17 +40,22 @@ class Corpus(object):
                     self.dictionary.add_word(word)
         print(f'dict length: {len(self.dictionary)}')
         max_len=800
-        # Tokenize file content
         with open(path, 'r', encoding="utf8") as f:
             idss = []
             for line in f:
+
                 # words = line.strip().split(' ') + ['<eos>']
-                words = line.strip().split(' ')
+                stripped = line.strip().split(' ')
+                if len(stripped) > (max_len - 2) or stripped[0] == '=':
+                    continue
+                words = ['<bos>'] + stripped + ['<eos>']
                 ids = []
                 for word in words:
                     ids.append(self.dictionary.word2idx[word])
-                remaining = [self.dictionary.word2idx['1']] * (max_len - len(ids))
-                idss.append(torch.tensor(ids+remaining).type(torch.int64))
+
+                assert len(ids) <= max_len - 2
+                remaining = [self.dictionary.word2idx[Corpus.padding]] * (max_len - len(ids))
+                idss.append(torch.tensor(ids+remaining).type(torch.long))
             ids = torch.cat(idss)
 
         return ids
